d033e92ad43b3b15efd290fd1776f8d4a10e1527
sraya691 <98817930+sraya691@users.noreply.github.com>
2022-06-08 18:12:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle= "grey";
+ctx.lineWidth= 1;
+ctx.rect()(x, y, width, height, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "blue";
+ctx.lineWidth= 5;
+ctx.arc()(x, y, 143 r, startAngle, endAngle);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "black";
+ctx.lineWidth= 1;
+ctx.rect()(x, y, width, height= 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth= 1;
+ctx.rect()(x, y, width, height= 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "orange";
+ctx.lineWidth= 1;
+ctx.rect()(x, y, width, height= 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "green";
+ctx.lineWidth= 1;
+ctx.rect()(x, y, width, height= 200);
+ctx.stroke();
